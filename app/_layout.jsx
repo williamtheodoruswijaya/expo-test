e6167fdf3ffcb24bf995de2812d4715a22990610
Admantix <williamwijaya499@gmail.com>
2024-05-10 15:45:17 +0700

    Another commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Slot, Stack } from "expo-router";
 
-export default function App() {
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Halo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
-}
+};
+
+export default RootLayout;
 
 const styles = StyleSheet.create({
   container: {
